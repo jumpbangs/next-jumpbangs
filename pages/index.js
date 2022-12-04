8cb0526dd8e68c27058412cdc0b2191913a06c1a
Amit Chongbang <amitchongbang@gmail.com>
2022-12-04 21:46:13 +0545

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+/**
+ *  Home Page
+ *
+ * @returns @Component
+ */
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
